e886aa56194562001f577914e07d167ea22e6547
juhnny <iwibest@naver.com>
2022-04-20 14:00:36 +0900

    new commit

--- app/src/main/java/com/juhnny/githubtest/MainActivity.kt
+++ app/src/main/java/com/juhnny/githubtest/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         
         var s = "이 문장은 깃헙 사이트에서 추가한 문장. Android Studio로 어떻게 가져가지?"
 
+        //처음 한 번만 등록해두면 그 다음부터는 Push/Pull만 하면 된다.
     }
 }
