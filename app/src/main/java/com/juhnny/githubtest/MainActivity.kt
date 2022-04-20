42bbae3e18dd59c217d1fa3c8fe800078305e956
juhnny <iwibest@naver.com>
2022-04-20 13:57:49 +0900

    Update MainActivity.kt

--- app/src/main/java/com/juhnny/githubtest/MainActivity.kt
+++ app/src/main/java/com/juhnny/githubtest/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportActionBar?.title = "GitHub 연동 테스트"
+        
+        var s = "이 문장은 깃헙 사이트에서 추가한 문장. Android Studio로 어떻게 가져가지?"
 
     }
 }
